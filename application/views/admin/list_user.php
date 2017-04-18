a381080d118145dbadcdd9707eaf6eeb4d316ac7
Badik Irwan <badikirwan@gmail.com>
2017-04-19 01:54:26 +0700

    Add files via upload

--- application/views/admin/list_user.php
+++ application/views/admin/list_user.php
@@ -1,6 +1,6 @@
 <div class="panel panel-flat">
 					<div class="panel-heading">
-						<h5 class="panel-title">Basic datatable</h5>
+						<h5 class="panel-title">Daftar User</h5>
 						<div class="heading-elements">
 							<ul class="icons-list">
 		                		<li><a data-action="collapse"></a></li>
@@ -17,58 +17,34 @@
 					<table class="table datatable-basic">
 						<thead>
 							<tr>
-								<th>No</th>
+								<th width="10%">No</th>
 								<th>Nim</th>
 								<th>Nama</th>
 								<th>Jurusan</th>
-								<th>Aksi</th>
+								<th width="15%">Aksi</th>
 							</tr>
 						</thead>
 						<tbody>
+						<?php 
+						$no = 0;
+							foreach ($data as $user) :
+							$no ++; ?>
 							<tr>
-								<td>...</td>
-								<td>...</td>
-								<td>...</td>
-								<td>...</td>
+								<td><?php echo $no; ?></td>
+								<td><?php echo $user['nim']; ?></td>
+								<td><?php echo $user['nama']; ?></td>
+								<td><?php echo $user['jurusan']; ?></td>
 								<td>
-									<div class="btn-group">
-		                    			<button type="button" class="btn btn-primary btn-icon dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-			                    			<i class="icon-menu7"></i> &nbsp;<span class="caret"></span>
-		                    			</button>
-
-		                    			<ul class="dropdown-menu dropdown-menu-right">
-											<li><a href="#"><i class="icon-menu7"></i> Action</a></li>
-											<li><a href="#"><i class="icon-screen-full"></i> Another action</a></li>
-											<li><a href="#"><i class="icon-mail5"></i> One more action</a></li>
-											<li class="divider"></li>
-											<li><a href="#"><i class="icon-gear"></i> Separated line</a></li>
-										</ul>
-									</div>
+									<div class="btn-group btn-group-animated">
+                                		<button type="button" class="btn btn-success dropdown-toggle" data-toggle="dropdown" aria-expanded="false">Action <span class="caret"></span></button>
+											<ul class="dropdown-menu animated bounceIn">
+												<li><a href="#"><i class="icon-menu7"></i> Detail</a></li>
+												<li><a href="#"><i class="icon-trash-alt"></i> Delete</a></li>
+											</ul>
+                            		</div>
                                 </td>
-
 							</tr>
-							<tr>
-								<td>...</td>
-								<td>...</td>
-								<td>...</td>
-								<td>...</td>
-								<td>
-									<div class="btn-group">
-		                    	<button type="button" class="btn btn-primary btn-icon dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-			                    	<i class="icon-menu7"></i> &nbsp;<span class="caret"></span>
-		                    	</button>
-
-		                    	<ul class="dropdown-menu dropdown-menu-right">
-									<li><a href="#"><i class="icon-menu7"></i> Action</a></li>
-									<li><a href="#"><i class="icon-screen-full"></i> Another action</a></li>
-									<li><a href="#"><i class="icon-mail5"></i> One more action</a></li>
-									<li class="divider"></li>
-									<li><a href="#"><i class="icon-gear"></i> Separated line</a></li>
-								</ul>
-							</div>
-                                </td>
-
-							</tr>
+						<?php endforeach ?>
 						</tbody>
 					</table>
 				</div>
